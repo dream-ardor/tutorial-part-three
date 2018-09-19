aae95562f3406f308dbf073d24e74884bb9dd541
Dan Barrick <barrick.dan80@gmail.com>
2018-09-18 18:31:18 -0700

    First commit

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
+    <br />
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+);
